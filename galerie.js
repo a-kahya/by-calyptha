1d5f5be7424ee5784ae65c8de45d17a9ac06aa56
a-kahya <ayse.kahya@live.fr>
2026-07-25 22:20:43 +0200

    ajout galerie

--- galerie.js
+++ galerie.js
@@ -11,16 +11,16 @@
 */
  
 const GALERIE = [
-  { id: "g1", photo: "images/galerie-1.jpg" },
-  { id: "g2", photo: "images/galerie-2.jpg" },
-  { id: "g3", photo: "images/galerie-3.jpg" },
-  { id: "g4", photo: "images/galerie-4.jpg" },
-  { id: "g5", photo: "images/galerie-5.jpg" },
-  { id: "g6", photo: "images/galerie-6.jpg" },
-  { id: "g7", photo: "images/galerie-7.jpg" },
-  { id: "g8", photo: "images/galerie-8.jpg" },
-  { id: "g9", photo: "images/galerie-9.jpg" },
-  { id: "g10", photo: "images/galerie-10.jpg" },
-  { id: "g11", photo: "images/galerie-11.jpg" },
-  { id: "g12", photo: "images/galerie-12.jpg" }
+  { id: "g1", photo: "images/galerie/1.jpg" },
+  { id: "g2", photo: "images/galerie/2.jpg" },
+  { id: "g3", photo: "images/galerie/3.jpg" },
+  { id: "g4", photo: "images/galerie/4.jpg" },
+  { id: "g5", photo: "images/galerie/5.jpg" },
+  { id: "g6", photo: "images/galerie/6.jpg" },
+  { id: "g7", photo: "images/galerie/7.jpg" },
+  { id: "g8", photo: "images/galerie/8.jpg" },
+  { id: "g9", photo: "images/galerie/9.jpg" },
+  { id: "g10", photo: "images/galerie/10.jpg" },
+  { id: "g11", photo: "images/galerie/11.jpg" },
+  { id: "g12", photo: "images/galerie/12.jpg" }
 ];
